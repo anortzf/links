ab26fafe507e4c3f13a058bece2263733387e942
anortzf <54248745+anortzf@users.noreply.github.com>
2021-08-21 17:48:26 -0300

    Update and rename single.js to single1.js

--- single1.js
+++ single1.js
@@ -2,9 +2,7 @@
 // https://cdn.jsdelivr.net/gh/anortzf/links/placelholder8.js
 
 [
-  {
-   
-    [
+
       {
         "img1": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27png%27&Quality=85&ImageId=%273239640%27&EntityType=%27Item%27&EntityId=%27227658%27&Width=237&Height=356&ImageUrl=%273239640.png%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
         "img2": "https://static.togglestatic.com/shain/v1/dataservice/ResizeImage/$value?Format=%27png%27&Quality=85&ImageId=%273239640%27&EntityType=%27Item%27&EntityId=%27227658%27&Width=237&Height=356&ImageUrl=%273239640.png%27&device=%27web_browser%27&subscriptions=%27Anonymous%27&segmentationTags=%27all%27&ResizeAction=%27fill%27&HorizontalAlignment=%27center%27&VerticalAlignment=%27top%27",
@@ -110,7 +108,5 @@
         "title": "Movie 1 ·90m· 2013",
         "link_s": "/Movie 1"
       }
-    ]
     
-  }
 ]
